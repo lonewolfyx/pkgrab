cefac3468d04426644f071e6ca70ac85a10b8d78
lonewolfyx <877085893@qq.com>
2026-01-29 14:48:47 +0800

    feat(cli): add pkgName parameter and simplify command action

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,14 @@
 import type { ICommandOptions } from '@/shared/types'
 import * as process from 'node:process'
 import cac from 'cac'
-import { resolveConfig } from '@/config.ts'
-import { inspectPackage, useMessage } from '@/utils'
 import { name, version } from '../package.json'
 
 const cli = cac(name)
 
-cli.command('')
+cli.command('[pkgName]')
     .option('--cwd,-c', 'project cwd', { default: process.cwd() })
-    .action(async (options: ICommandOptions) => {
-        const config = resolveConfig(options)
-
-        const { hasPackage } = await inspectPackage(config)
-        if (!hasPackage) {
-            return useMessage('Could not find a package.json file in the current directory or its parents.', {
-                borderColor: 'red',
-            })
-        }
+    .action(async (pkgName: string = '', options: ICommandOptions) => {
+        console.log('pkgName', pkgName)
     })
 
 cli.help()
